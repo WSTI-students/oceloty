21e57540643b2a42a7e91a885240140954683f3e
Husky7 <bartlomiejaller@gmail.com>
2019-07-07 13:10:40 +0200

    remove unnecessary empty lines

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,8 +1,5 @@
 class Shelter < ApplicationRecord
   validates :name, :adress, presence: true
-
   validates :phone_number, format: { with: /(\+48\s)?\d{3}\s?\d{3}\s?\d{3}/,
                                      message: "Zły format numeru telefonu!!!" }
-
-  
 end
