b7c4ef3aeff24953d6ce6823953aa52b8c88ef08
Husky7 <bartlomiejaller@gmail.com>
2019-07-07 14:16:34 +0200

    Fix phone number validates

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,5 +1,5 @@
 class Shelter < ApplicationRecord
   validates :name, :adress, presence: true
-  validates :phone_number, format: { with: /(\+48\s)?\d{3}\s?\d{3}\s?\d{3}/,
-                                     message: "Zły format numeru telefonu!!!" }
+  validates :phone_number, format: { with: /(\+\d{1,3}\s)?\d{3}\s?\d{3}\s?\d{3}/,
+                                     message: "Zły format numeru telefonu :(" }
 end
