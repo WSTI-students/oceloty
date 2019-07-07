19d7cc718c5b0e7256f1c6511be331cef1259d1d
Husky7 <bartlomiejaller@gmail.com>
2019-07-07 14:27:34 +0200

    Second fix phone number

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,5 +1,5 @@
 class Shelter < ApplicationRecord
   validates :name, :adress, presence: true
-  validates :phone_number, format: { with: /(\+\d{1,3}\s)?\d{3}\s?\d{3}\s?\d{3}/,
+  validates :phone_number, format: { with: /\A(\+\d{1,3}\s)?\d{3}\s?\d{3}\s?\d{3}\z/,
                                      message: "Zły format numeru telefonu :(" }
 end
