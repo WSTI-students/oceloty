7edcbd370e53479da434e85642b0c29c29059824
emkrz <emkrzeszowska@gmail.com>
2019-07-12 19:33:02 +0200

    Add pet-form and views

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,4 +1,35 @@
 class PetsController < ApplicationController
   def index
+    @pet = Pet.all
+  end
+
+
+  def new
+    @pet = Pet.new
+  end
+
+
+def create
+  @pet = Pet.new(pet_params)
+
+  respond_to do |format|
+    if @pet.save
+      format.html { redirect_to @pet, notice: 'Pet was successfully created.' }
+      format.json { render :show, status: :created, location: @pet }
+    else
+      format.html { render :new }
+      format.json { render json: @pet.errors, status: :unprocessable_entity }
+    end
   end
 end
+
+private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_pet
+      @pet = Pet.find(params[:id])
+    end
+
+    def pet_params
+      params.require(:pet).permit(:name, :sex, :birth_date, :breed, :species, :size, :registry_id, :description, :children_friendly, :allergic_friendly, :dog_friendly, :sterilized, :microchipped)
+    end
+  end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,7 @@
 class Pet < ApplicationRecord
+  BOOLEAN_OPTION = { 'Tak' => true, 'Nie' => false, 'Nie wiadomo' => nil }.freeze
+  CLASS_pets_sex = ['samiec', 'samica']
+  CLASS_pets_size = ['mały', 'średni', 'duzy']
   validates :name, :sex, :breed, :species, presence: true
   belongs_to :shelter
 end
