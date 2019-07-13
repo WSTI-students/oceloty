b7a4ec2c394826df8f88c8a7f7d1039cb8d0b550
emkrz <emkrzeszowska@gmail.com>
2019-07-13 09:00:27 +0200

    Change form

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,8 +1,11 @@
 class PetsController < ApplicationController
+  before_action :set_pet, only: [:show, :edit, :update, :destroy]
+
   def index
     @pet = Pet.all
   end
 
+  def show; end
 
   def new
     @pet = Pet.new
@@ -30,6 +33,6 @@ private
     end
 
     def pet_params
-      params.require(:pet).permit(:name, :sex, :birth_date, :breed, :species, :size, :registry_id, :description, :children_friendly, :allergic_friendly, :dog_friendly, :sterilized, :microchipped)
+      params.require(:pet).permit(:name, :sex, :birth_date, :breed, :species, :size, :registry_id, :age_info, :description, :children_friendly, :allergic_friendly, :dog_friendly, :cat_friendly, :sterilized, :microchipped)
     end
   end
--- app/views/pets/pet.json.jbuilder
+++ app/views/pets/pet.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! pet, :id, :name, :sex, :birth_date, :children_friendly, :allergic_friendly, :dog_friendly, :sterilized, :microchipped, :created_at, :updated_at
+json.extract! pet, :id, :name, :sex, :birth_date, :children_friendly, :allergic_friendly, :dog_friendly, :cat_friendly, :sterilized, :microchipped, :created_at, :updated_at
 json.url pet_url(pet, format: :json)
