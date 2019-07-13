68d4c63d346aa1c8dd06d80a7c5a3d60f8c8c19b
jerryskye <jarek@siedlarz.com>
2019-07-13 14:05:07 +0200

    Fix errors for registrations

--- app/controllers/shelters/registrations_controller.rb
+++ app/controllers/shelters/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Shelters::RegistrationsController < Devise::RegistrationsController
   include Accessible
   skip_before_action :check_user, except: [:new, :create]
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
