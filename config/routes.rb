1ebf219b890dc629d1b757bdcd4e4400ced04164
Husky7 <bartlomiejaller@gmail.com>
2019-07-13 10:08:09 +0200

    Add devise to shelter

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: 'users', controllers: { sessions: "users/sessions",
+                                                   unlock: "users/unlocks",
+                                                   registrations: "users/registrations",
+                                                   passwords: "users/passwords",
+                                                   confirmations: "users/confirmations" }
+
+  devise_for :shelters, path: 'shelters', controllers: { sessions: "shelters/sessions",
+                                                         unlock: "shelters/unlocks",
+                                                         registrations: "shelters/registrations",
+                                                         passwords: "shelters/passwords",
+                                                         confirmations: "shelters/confirmations" }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pets#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_12_172638) do
+ActiveRecord::Schema.define(version: 2019_07_13_062103) do
 
   create_table "pets", force: :cascade do |t|
     t.string "name"
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 2019_07_12_172638) do
     t.string "account_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_shelters_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_shelters_on_reset_password_token", unique: true
   end
 
   create_table "users", force: :cascade do |t|
