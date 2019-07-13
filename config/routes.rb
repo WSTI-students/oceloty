4d7c91f53f8eb2d1eb7dac0c251fce6e6d193166
jerryskye <jarek@siedlarz.com>
2019-07-13 11:29:34 +0200

    Remove unnecessary Devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, path: 'users', controllers: { sessions: "users/sessions",
-                                                   unlock: "users/unlocks",
                                                    registrations: "users/registrations",
-                                                   passwords: "users/passwords",
-                                                   confirmations: "users/confirmations" }
+                                                   passwords: "users/passwords" }
 
   devise_for :shelters, path: 'shelters', controllers: { sessions: "shelters/sessions",
-                                                         unlock: "shelters/unlocks",
                                                          registrations: "shelters/registrations",
-                                                         passwords: "shelters/passwords",
-                                                         confirmations: "shelters/confirmations" }
+                                                         passwords: "shelters/passwords" }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pets#index'
