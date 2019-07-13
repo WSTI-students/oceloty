c5e95859a02197ce5fd4306c8a95ae6fa06090f0
emkrz <emkrzeszowska@gmail.com>
2019-07-13 14:10:52 +0200

    add image to form

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,28 +11,36 @@ class PetsController < ApplicationController
     @pet = Pet.new
   end
 
+  def create
+    # tempolary solution
+    shelter = Shelter.find_or_create_by!(name: "Schronisko", address: "Ul",
+      phone_number: "531 519 111")
+    @pet = shelter.pets.new(pet_params)
 
-def create
-  @pet = Pet.new(pet_params)
-
-  respond_to do |format|
-    if @pet.save
-      format.html { redirect_to @pet, notice: 'Pet was successfully created.' }
-      format.json { render :show, status: :created, location: @pet }
-    else
-      format.html { render :new }
-      format.json { render json: @pet.errors, status: :unprocessable_entity }
+    respond_to do |format|
+      if @pet.save
+        format.html { redirect_to @pet, notice: 'Pet was successfully created.' }
+        format.json { render :show, status: :created, location: @pet }
+      else
+        format.html { render :new }
+        format.json { render json: @pet.errors, status: :unprocessable_entity }
+      end
     end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
+
+  def pet_params
+    params.require(:pet).permit(
+      :image, :name, :sex, :birth_date, :breed, :species, :size,
+      :registry_id, :age_info, :description, :children_friendly,
+      :allergic_friendly, :dog_friendly, :cat_friendly, :sterilized,
+      :microchipped
+    )
   end
 end
-
-private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_pet
-      @pet = Pet.find(params[:id])
-    end
-
-    def pet_params
-      params.require(:pet).permit(:name, :sex, :birth_date, :breed, :species, :size, :registry_id, :age_info, :description, :children_friendly, :allergic_friendly, :dog_friendly, :cat_friendly, :sterilized, :microchipped)
-    end
-  end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,8 +1,13 @@
 class Pet < ApplicationRecord
   BOOLEAN_OPTION = { 'Tak' => true, 'Nie' => false, 'Nie wiadomo' => nil }.freeze
+
   enum sex: %w[samiec samica].freeze
   enum size: %w[mały średni duży].freeze
   enum species: %w[kot pies].freeze
-  validates :name, :sex, :breed, :species, presence: true
+
   belongs_to :shelter
+
+  has_one_attached :image
+
+  validates :name, :sex, :breed, :species, presence: true
 end
