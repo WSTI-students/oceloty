22b5f64d063aa375a76d293cb9666712484212db
jerryskye <jarek@siedlarz.com>
2019-07-13 14:29:53 +0200

    Add routes for new shelter pets action

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,9 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pets#index'
+
+  # TODO: add Shelters::PetsController
+  resources :shelters, only: [] do
+    resources :pets, only: [:index], controller: 'shelters/pets'
+  end
 end
