637e3f7e81b2e6ab38c61988f1aa2fd06fa26f8c
Husky7 <bartlomiejaller@gmail.com>
2019-07-13 13:00:00 +0200

    Sign up edit

--- app/controllers/shelters/registrations_controller.rb
+++ app/controllers/shelters/registrations_controller.rb
@@ -40,12 +40,12 @@ class Shelters::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone_number, :address])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
