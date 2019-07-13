211526abba80d3934cf54e71bc2fbe9a511533a0
jerryskye <jarek@siedlarz.com>
2019-07-13 14:15:17 +0200

    Fix Accessible concern
    It will redirect logged in users to list of pets
    and logged in shelters to their pets

--- app/controllers/concerns/accessible.rb
+++ app/controllers/concerns/accessible.rb
@@ -8,12 +8,10 @@ module Accessible
   def check_user
     if current_shelter
       flash.clear
-      # if you have rails_shelter. You can redirect anywhere really
-      redirect_to(rails_shelter.dashboard_path) && return
+      redirect_to(shelter_pets_path(current_shelter)) && return
     elsif current_user
       flash.clear
-      # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
-      redirect_to(authenticated_user_root_path) && return
+      redirect_to(pets_path) && return
     end
   end
 end
